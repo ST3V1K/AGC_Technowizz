a6c01bfdced82624d23b0cda3beb3e2d3928a831
ST3V1K <j.nekvind@seznam.cz>
2022-01-26 18:51:00 +0100

    Přidání testování plnosti, vytvoření souboru data.csv a přesunutí souboru Config.conf

--- Core.cs
+++ Core.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace AGC_Technowizz {
   static public class Core {
     private static string CONFIG = "";
-    private static readonly int MAX_SIZE = 24;
-
+    const string FullErrorMessage = "Plno";
     public static Dictionary<string, int[]> Storage = new Dictionary<string, int[]>();
 
-    public static void Setup(string path = @"../../Config.conf")
+    public static void Setup(string path = @"./Config.conf")
     {
-      // Možná zde nahrát a použít data z excelového souboru (pro ukázku programu), který jsme dostali
-
-      System.IO.StreamReader sr = new System.IO.StreamReader(path);
-
+      // Vytvoření configu
+      StreamReader sr = new StreamReader(path);
       foreach (string line in sr.ReadToEnd().Split('\n'))
         if (line.StartsWith("#") || line.Length <= 3) continue;
         else CONFIG += line + "\n";
-
       sr.Close();
+
+      // Načtení plnosti skladu
+      LoadDataFromDatabase();
     }
 
     public static string GetCarBrandFromContainerCode(string containerCode) {
@@ -39,6 +38,7 @@ namespace AGC_Technowizz {
     public static string GetZoneFromCarBrand(string carBrand) {
       Dictionary<string, string> carZonePairs = new Dictionary<string, string>();
 
+      // Načtení configu
       foreach (string line in CONFIG.Split('\n'))
       {
         if (line.Length > 0)
@@ -48,53 +48,75 @@ namespace AGC_Technowizz {
         }
       }
 
+      // Pokud je definováno více zón
       if (carZonePairs[carBrand.ToUpper()].Contains(","))
       {
         string[] zones = carZonePairs[carBrand.ToUpper()].Split(',');
-        int zoneIndex = 0;
         bool full;
-        do
-        {
-          try
-          {
-            full = IsZoneFull(zones[zoneIndex]);
-          } 
-          catch (IndexOutOfRangeException)
-          {
-            return "FULL";
-          }
-          if (full) zoneIndex++;
-        } while (full);
+        string zone = "";
 
-        AddToStorage(zones[zoneIndex]);
-        return zones[zoneIndex];
+        foreach (string _zone in zones)
+        {
+          zone = _zone;
+          full = IsZoneFull(zone);
+          if (!full)
+          {
+            // Alespoň jedna zóna není plná (definováno více zón)
+            AddToStorage(zone);
+            return zone;
+          }
+        }
+        // Všechny zóny jsou (definováno více zón)
+        return $"{FullErrorMessage}-{zone}";
       }
-      AddToStorage(carZonePairs[carBrand.ToUpper()]);
-      return carZonePairs[carBrand.ToUpper()];
+
+      if (!IsZoneFull(carZonePairs[carBrand.ToUpper()]))
+      {
+        // Není plná zóna (pouze 1 definována)
+        AddToStorage(carZonePairs[carBrand.ToUpper()]);
+        return carZonePairs[carBrand.ToUpper()];
+      }
+      // Je plná zóna (pouze 1 definována)
+      return $"{FullErrorMessage}-{carZonePairs[carBrand.ToUpper()]}";
     }
 
     public static bool IsZoneFull(string zone)
     {
-      try
-      {
-        if (Storage[zone][0] >= Storage[zone][1]) return true;
-      }
-      catch (KeyNotFoundException)
-      {
-        Storage.Add(zone, new int[] { 1, MAX_SIZE });
-      }
+      zone = zone.Trim().ToUpper();
+
+      // Testování plnosti zóny
+      if (Storage[zone][0] >= Storage[zone][1]) return true;
       return false;
     }
 
     private static void AddToStorage(string key)
     {
-      try
+      key = key.Trim().ToUpper();
+
+      // Přidání do skladu (z důvodu výpočtu plnosti)
+      Storage[key][0]++;
+    }
+
+    public static void LoadDataFromDatabase(string Cesta_k_souboru = @"./data.csv")
+    {
+      using (StreamReader sr = new StreamReader(Cesta_k_souboru))
       {
-        Storage[key][0]++;
-      } 
-      catch (KeyNotFoundException)
-      {
-        Storage.Add(key, new int[] { 0, MAX_SIZE });
+        /*
+         * 
+         * Zde je zapotřebí výstup z databáze
+         * soubor data.csv ve formátu [název zóny];[aktuální stav zóny];[maximalní počet palet v zóně]
+         * 
+         */
+
+        // Nahrání souboru do plnosti skladu
+        while (!sr.EndOfStream)
+        {
+          string[] items = sr.ReadLine().Split(';');
+          if (Storage.ContainsKey(items[0])) 
+            Storage[items[0].ToUpper().Trim()] = new int[] { Convert.ToInt32(items[1]), Convert.ToInt32(items[2]) };
+          else 
+            Storage.Add(items[0].ToUpper().Trim(), new int[] { Convert.ToInt32(items[1]), Convert.ToInt32(items[2]) });
+        }
       }
     }
   }
